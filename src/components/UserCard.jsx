bc9cbefa169cd996a45585843aaa374ad80d849a
ghana01 <ghanshyammishra58258@gmail.com>
2025-12-16 16:06:40 +0530

    intrigate the connection

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { removeFeed } from '../utils/feedSlice';
 
 const UserCard = ({ user }) => {
+
+    const dispatch = useDispatch();
   console.log("user card rendered", user);
 
   // Safety check
   if (!user) return null;
+  
 
-  const { firstName, lastName, photoUrl, age, gender, about } = user;
+  const { _id,firstName, lastName, photoUrl, age, gender, about } = user;
+     const handelsendRequest = async (status,_id)=>{
+        await axios.post(`http://localhost:4000/request/send/${status}/${_id}`,
+            {},{withCredentials:true});
 
+        alert(`Request ${status} successfully`);
+        
+        dispatch(removeFeed(_id));
+
+     }
   return (
     <div className="card bg-blue-300 w-96 shadow-xl">
       <figure>
@@ -25,8 +39,10 @@ const UserCard = ({ user }) => {
         <p>{about}</p>
         
         <div className="card-actions justify-center my-4">
-          <button className="btn btn-secondary">Ignore</button>
-          <button className="btn btn-primary">Interested</button>
+          <button
+          onClick={() => handelsendRequest('ignore', _id)}
+          className="btn btn-secondary">Ignore</button>
+          <button onClick={() => handelsendRequest('interested', _id)} className="btn btn-primary">Interested</button>
         </div>
       </div>
     </div>
--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -1,18 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const feedSlice = createSlice({
-    name:'feed',
-    initialState:null,
-    reducers:{
-        addFeed:(state,action)=>{
-            return action.payload
+    name: 'feed',
+    initialState: null,
+    reducers: {
+        addFeed: (state, action) => {
+            return action.payload;
         },
-        removeFeed:(state,action)=>{
-            return null;
+        removeFeed: (state, action) => {
+            // LOGIC: Filter out the user whose _id matches the action.payload
+            const newFeed = state.filter((user) => user._id !== action.payload);
+            return newFeed;
         }
     },
 });
 
-
-export const {addFeed,removeFeed} = feedSlice.actions;
+export const { addFeed, removeFeed } = feedSlice.actions;
 export default feedSlice.reducer;
